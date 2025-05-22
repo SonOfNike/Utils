5736b72411bf918809d5eb036fea3c08eb65f228
Chris Freeman <chrisfreeman20@gmail.com>
2025-05-22 09:39:25 -0500

    add shared mem names

--- globals.h
+++ globals.h
@@ -5,3 +5,7 @@ const extern int DOLLAR = 100 * CENTS;
 const extern int MD_QUEUE_SIZE = 1024;
 const extern int RESP_QUEUE_SIZE = 1024;
 const extern int REQ_QUEUE_SIZE = 1024;
+
+const char* MD_shm_name = "/md_shared_memory";
+const char* RESP_shm_name = "/resp_shared_memory";
+const char* REQ_shm_name = "/req_shared_memory";
