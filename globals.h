81516ac8422d05b7e2c82d1b5283e4b3e08e117d
SonOfNike <chrisfreeman.trades@gmail.com>
2025-06-12 22:34:52 +0000

    add global times

--- globals.h
+++ globals.h
@@ -8,6 +8,12 @@ static const int32_t MD_QUEUE_SIZE = 1024;
 static const int32_t RESP_QUEUE_SIZE = 1024;
 static const int32_t REQ_QUEUE_SIZE = 1024;
 
+static const Timestamp MICRO_SECONDS = 1000;
+static const Timestamp MILLI_SECONDS = 1000 * MICRO_SECOND;
+static const Timestamp SECONDS = MILLI_SECONDS * 1000;
+static const Timestamp MINUTES = SECONDS * 60;
+static const Timestamp HOURS = MINUTES * 60;
+
 static const char* MD_shm_name = "/md_shared_memory";
 static const char* RESP_shm_name = "/resp_shared_memory";
 static const char* REQ_shm_name = "/req_shared_memory";
