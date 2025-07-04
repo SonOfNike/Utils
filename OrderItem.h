eb6cdf8125725bc3ffad45f8a017963f1b03e6b1
SonOfNike <chrisfreeman.trades@gmail.com>
2025-07-04 18:38:11 +0000

    OrderItem update

--- OrderItem.h
+++ OrderItem.h
@@ -4,10 +4,24 @@
 
 enum class order_state {PENDING_NEW,PENDING_CANCEL,PENDING_MODIFY,CONFIRMED,NONE};
 
-struct OrderItem{
+class OrderItem{
+public:
     Price m_order_price = 0;
     Shares m_order_quant = 0;
     Shares m_total_fills = 0;
     OrderId m_id = 0;
+    side m_side = side::NONE;
     order_state m_state = order_state::NONE;
+
+    void setPrice(Price _price){
+        m_order_price = _price;
+    }
+
+    void setQuant(Shares _quant){
+        m_order_quant = _quant;
+    }
+
+    void setSide(side _side){
+        m_side = _side;
+    }
 };
